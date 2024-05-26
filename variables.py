64319477dd4e571cec1773569b02e3b551597b93
John <john@0x15.page>
2024-05-25 22:11:24 -0700

    More Bro Code

---
+++ variables.py
@@ -0,0 +1,24 @@
+# variable is a container for a value
+
+first_name = "Bro"
+last_name = "Code"
+full_name = first_name + " " +last_name
+
+# print <class 'str'>
+# print(type(first_name))
+
+print("Hello " + full_name)
+
+
+age = 21
+age += 1
+# print(age)
+
+print("Your age is: " + str(age))
+
+height = 250.0
+print(height)
+print(type(height))
+
+
+
